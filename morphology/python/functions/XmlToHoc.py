3df2a15eb2165123c61dbf334a27a02d13b67fa5
alex sutton <acsutt0n@gmail.com>
2015-02-19 16:53:56 -0500

    2.19.2015c

--- morphology/python/functions/XmlToHoc.py
+++ morphology/python/functions/XmlToHoc.py
@@ -15,11 +15,16 @@ class SkelHoc():
     self.branchpoints = []
     self.branchset = None
     self.segments = {'*': None}
-    self.connections = {'*': None}
+    self.connections = []
     self.sources, self.targets = None, None
     
     # now do shit
     self.read_xml()
+    self.create_segments()
+    self.create_connections()
+  
+  
+  
   
   ####### Read File #######
   def add_node(self, line):
@@ -64,15 +69,22 @@ class SkelHoc():
     Run through edges, nodes and branchpoints to return the actual
     segments. 
     """
+    print('Creating segments ...')
     # start with a random edge (or node)
     self.sources = [i['source'] for i in self.edges]
     self.targets = [i['target'] for i in self.edges]
-    Edges = zip(sources, targets)
     self.branchset = self.branchpoints*2
+    self.branchset = [int(i) for i in self.branchset]
+    # print(self.branchset)
     
-    while len(self.branchpoints) > 0:
-      rand_ind = int(np.random.random(1)*len(self.branchpoints
-      self.
+    while len(self.branchset) > 0:
+      print('%i branchpoints remaining ...' %len(self.branchset))
+      rand_ind = int(np.random.random(1)*len(self.branchset))
+      self.crawl(int(self.branchset[rand_ind]))
+      
+    return self
+  
+  
     
   def crawl(self, b):
     """
@@ -83,7 +95,10 @@ class SkelHoc():
     if b in self.sources or b in self.targets:
       go = True
     else:
-      self.branchset.pop(self.branchset.index(b))
+      try:
+        self.branchset.pop(self.branchset.index(b))
+      except:
+        print()
       return
     current = b
     while go:
@@ -93,19 +108,21 @@ class SkelHoc():
         nodes.append(current)
         self.sources.pop(ind)
         self.targets.pop(ind)
-      if current in self.targets and current not in self.branchpoints:
+      elif current in self.targets and current not in self.branchpoints:
         ind = self.targets.index(current)
         current = self.sources[ind]
         nodes.append(current)
         self.sources.pop(ind)
         self.targets.pop(ind)
-      if current in self.branchpoints:
+      elif current in self.branchpoints:
         go = False
         try:
           self.branchset.pop(self.branchset.index(current))
         except:
           print('Tried to pop %i from branchset but was already popped'
                 % current)
+      else:
+        go = False
     # end of while loop
     if nodes[0] not in self.branchpoints or nodes[-1] not in self.branchpoints:
       print('Segment does not start and end with branchpoints:')
@@ -116,43 +133,60 @@ class SkelHoc():
   
 
   def format_segment(self, nodes):
-    self.segments[str(len(self.segments.keys()))] = {}
+    newseg = len(self.segments.keys())
+    self.segments[str(newseg)] = {}
     for n in range(len(nodes)):
-      self.segments[str(len(self.segments.keys()))][str(n)] = \
+      self.segments[str(newseg)][str(n)] = \
         self.nodes[str(nodes[n])]
     return self
-    
-    
   
   
-
-class Segment():
-  def __init__(self, branchpoints, all_nodes, edges, n):
-    # n is the current branchpoint number
-    self.branchpoints = branchpoints
-    self.all_nodes = all_nodes
-    self.edges = edges
-    self.n = n
-    
   
-  
-  def node_crawl(self):
-    """
-    Go from one branchpoint to the next.
-    """
-    
-
-
-
-
-class Node():
-  def __init__(self, name, x, y, z, rad):
-    self.name = name
-    self.x = x
-    self.y = y
-    self.z = z
-    self.rad = rad
+  def create_connections(self):
+    end_segs, end0s, end1s = [], [], []
+    if '*' in self.segments.keys():
+      self.segments.pop('*')
+    for c in self.segments.keys():
+      end_segs.append(c)
+      end0s.append(self.segments[c]['0'])
+      last = len(self.segments[c])-1
+      end1s.append(self.segments[c][str(last)])
+    for e in range(len(end_segs)):
+      if end1s[e] in end0s:
+        # found a segment that connects to e's 1th end
+        f = end0s.index(end1s[e])
+        newcon = {'0': end_segs[f], '1': end_segs[e]}
+        if newcon not in self.connections:
+          self.connections.append(newcon)
+      if end0s[e] in end1s:
+        # found a segment that connects to e's 0th end
+        f = end1s.index(end0s[e])
+        newcon = {'0': end_segs[e], '1': end_segs[f]}
+        if newcon not in self.connections:
+          self.connections.append(newcon)
+    # end of for loop
     return self
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
